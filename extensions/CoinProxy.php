5b429dba26cd40db706cb7614d57627722b19be2
Mikk Tendermann <mikk150@gmail.com>
2017-02-19 21:31:47 +0200

    make ping pong

--- extensions/CoinProxy.php
+++ extensions/CoinProxy.php
@@ -22,15 +22,9 @@ class CoinProxy implements MessageComponentInterface
 
     public function onMessage(ConnectionInterface $from, $msg)
     {
-        $numRecv = count($this->clients) - 1;
-        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
-            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
-
-        foreach ($this->clients as $client) {
-            if ($from !== $client) {
-                // The sender is not the receiver, send to each client connected
-                $client->send($msg);
-            }
+        if ($msg === 'PING') {
+            $from->send('PONG');
+            echo "PING from {$from->resourceId}";
         }
     }
 
